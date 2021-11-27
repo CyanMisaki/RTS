8566a13a5fd114b9dc391e60a91323d3508ea497
CyanMisaki <fluffyshy@yandex.ru>
2021-11-27 21:11:59 +0300

    transferring modules

--- Assets/Scripts/UserControlSystem/UI/Presenter/MouseInteractionPresenter.cs
+++ Assets/Scripts/UserControlSystem/UI/Presenter/MouseInteractionPresenter.cs
@@ -3,65 +3,67 @@ using System.Linq;
 using Abstractions;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UserControlSystem;
 using UserControlSystem.UI.Model;
 using Utils;
 
-public sealed class MouseInteractionPresenter : MonoBehaviour
+namespace UserControlSystem.UI.Presenter
 {
-    [SerializeField] private Camera _camera;
-    [SerializeField] private SelectableValue _selectedObject;
-    [SerializeField] private EventSystem _eventSystem;
-    
-    [SerializeField] private Vector3Value _groundClicksRMB;
-    [SerializeField] private AttackableValue _attackablesRMB;
-    [SerializeField] private Transform _groundTransform;
-    
-    private Plane _groundPlane;
-    
-    private void Start() => _groundPlane = new Plane(_groundTransform.up, 0);
-
-    private void Update()
+    public sealed class MouseInteractionPresenter : MonoBehaviour
     {
-        if (!Input.GetMouseButtonUp(0) && !Input.GetMouseButton(1))
-        {
-            return;
-        }
-        
-        if (_eventSystem.IsPointerOverGameObject())
-        {
-            return;
-        }
-        
-        var ray = _camera.ScreenPointToRay(Input.mousePosition);
-        var hits = Physics.RaycastAll(ray);
-        if (Input.GetMouseButtonUp(0))
-        {
-            _selectedObject.SetValue(HittedObject<ISelectable>(hits, out var selectable) ? selectable : null);
-        }
-        else
-        {
-            if (HittedObject<IAttackable>(hits, out var attackable))
-            {
-                _attackablesRMB.SetValue(attackable);
-            }
-            else if (_groundPlane.Raycast(ray, out var enter))
-            {
-                _groundClicksRMB.SetValue(ray.origin + ray.direction * enter);
-            }
-        }
-    }
+        [SerializeField] private Camera _camera;
+        [SerializeField] private SelectableValue _selectedObject;
+        [SerializeField] private EventSystem _eventSystem;
+    
+        [SerializeField] private Vector3Value _groundClicksRMB;
+        [SerializeField] private AttackableValue _attackablesRMB;
+        [SerializeField] private Transform _groundTransform;
+    
+        private Plane _groundPlane;
+    
+        private void Start() => _groundPlane = new Plane(_groundTransform.up, 0);
 
-    private bool HittedObject<T>(IReadOnlyCollection<RaycastHit> hits, out T result) where T : class
-    {
-        result = default;
-        if (hits.Count == 0)
+        private void Update()
         {
-            return false;
-        }    
-        result = hits
-            .Select(hit => hit.collider.GetComponentInParent<T>())
-            .FirstOrDefault(item => item != null);
-        return result != default;
+            if (!Input.GetMouseButtonUp(0) && !Input.GetMouseButton(1))
+            {
+                return;
+            }
+        
+            if (_eventSystem.IsPointerOverGameObject())
+            {
+                return;
+            }
+        
+            var ray = _camera.ScreenPointToRay(Input.mousePosition);
+            var hits = Physics.RaycastAll(ray);
+            if (Input.GetMouseButtonUp(0))
+            {
+                _selectedObject.SetValue(HittedObject<ISelectable>(hits, out var selectable) ? selectable : null);
+            }
+            else
+            {
+                if (HittedObject<IAttackable>(hits, out var attackable))
+                {
+                    _attackablesRMB.SetValue(attackable);
+                }
+                else if (_groundPlane.Raycast(ray, out var enter))
+                {
+                    _groundClicksRMB.SetValue(ray.origin + ray.direction * enter);
+                }
+            }
+        }
+
+        private bool HittedObject<T>(IReadOnlyCollection<RaycastHit> hits, out T result) where T : class
+        {
+            result = default;
+            if (hits.Count == 0)
+            {
+                return false;
+            }    
+            result = hits
+                .Select(hit => hit.collider.GetComponentInParent<T>())
+                .FirstOrDefault(item => item != null);
+            return result != default;
+        }
     }
 }
